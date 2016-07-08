e4a0b04b7fcea3ae92b31099ea14025222af4a2d
Oscar Persson <dallas.persson@gmail.com>
2016-07-08 10:37:43 +0200

    Add tweepy for Twitter API integration

--- app.py
+++ app.py
@@ -3,15 +3,21 @@
 from lxml import html
 import requests
 import re
+import os
+import tweepy
+
+auth = tweepy.OAuthHandler(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
+
+tweepy = tweepy.API(auth)
 
 page = requests.get('https://en.wikipedia.org/wiki/Main_Page')
 tree = html.fromstring(page.content)
 
-#This will create a list of stories:
 stories = tree.xpath('//div[@id="mp-otd"]/ul[1]/li')
 
-tweets = []
 for story in stories:
-    tweets.append(re.sub('<[^<]+>|\\n', "", html.tostring(story, encoding="ascii")).replace("&#8211;", "-"))
-
-print 'Tweets: ', tweets
+    story = re.sub('<[^<]+>|\\n', "", html.tostring(story, encoding="ascii")).replace("&#8211;", "-")
+    if len(story) <= 140:
+        tweepy.update_status(story)
+        break
