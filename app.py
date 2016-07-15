6dad969ff9907be8961955261904cba851053408
Oscar Persson <dallas.persson@gmail.com>
2016-07-15 09:33:56 +0200

    Randomize fact being tweeted

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import re
 import os
 import tweepy
 import time
+import random
 
 class DailyWikiBot:
     def __init__(self):
@@ -13,15 +14,18 @@ class DailyWikiBot:
         auth.set_access_token(os.environ['ACCESS_TOKEN'], os.environ['ACCESS_TOKEN_SECRET'])
         self.tweepy = tweepy.API(auth)
 
-    def run(self):
-        page = requests.get('https://en.wikipedia.org/wiki/{0}_{1}'.format(time.strftime('%B'), time.strftime('%d')))
-        facts = html.fromstring(page.content).xpath('//div[@id="mw-content-text"]/ul[1]/li')
-
+    def parse(self, facts):
+        parsedFacts = []
         for fact in facts:
             fact = re.sub('<[^<]+>|\\n', '', html.tostring(fact, encoding='UTF-8'))
-            if len(fact) <= 140:
-                self.tweepy.update_status(fact)
-                break
+            if len(fact) <= 140: parsedFacts.append(fact)
+        return parsedFacts
+
+    def run(self):
+        page = requests.get('https://en.wikipedia.org/wiki/{0}_{1}'.format(time.strftime('%B'), time.strftime('%d')))
+        facts = self.parse(html.fromstring(page.content).xpath('//div[@id="mw-content-text"]/ul[1]/li'))
+
+        self.tweepy.update_status(random.choice(facts))
 
 
 bot = DailyWikiBot()
